0264ce601b7dd032021bb08f092ff2032c39cda6
veranyagaka <nyagakaver@gmail.com>
2024-07-11 23:40:36 +0300

    fixed reports

--- backend/routes/reports.js
+++ backend/routes/reports.js
@@ -5,19 +5,32 @@ const database = require('../db.js');
 
 router.get('/', async (req, res) => {
   try {
-    const [femaleEmployees] = await database.query('SELECT COUNT(*) as count FROM employee_test WHERE gender = "F"');
+    const [femaleEmployees] = await database.query('SELECT COUNT(*) as count FROM employee_profile WHERE gender = "F"');
     console.log('Female Employees:', femaleEmployees[0].count); 
-    const [maleEmployees] = await database.query('SELECT COUNT(*) as count FROM employee_test WHERE gender = "M"');
-    const [availableEmployees] = await database.query('SELECT COUNT(*) as count FROM employee_test WHERE availability = "Available"');
+    const [maleEmployees] = await database.query('SELECT COUNT(*) as count FROM employee_profile WHERE gender = "M"');
+    const [notEmployees] = await database.query('SELECT COUNT(*) as count FROM employee_profile WHERE gender = "Prefer not to say"');
+
+    const [availableEmployees] = await database.query('SELECT COUNT(*) as count FROM employee_profile WHERE availability = "Available"');
     console.log('Available Employees:', availableEmployees[0].count); 
 
-    const [onLeaveEmployees] = await database.query('SELECT COUNT(*) as count FROM employee_test WHERE availability = "On Leave"');
+    const [onLeaveEmployees] = await database.query('SELECT COUNT(*) as count FROM employee_profile WHERE availability = "On Leave"');
+
+    // Fetch department counts
+    const [recruitmentCount] = await database.query('SELECT COUNT(*) as count FROM employee_profile WHERE department = "Recruitment"');
+    const [complianceCount] = await database.query('SELECT COUNT(*) as count FROM employee_profile WHERE department = "Compliance"');
+    const [compensationCount] = await database.query('SELECT COUNT(*) as count FROM employee_profile WHERE department = "Compensation"');
+    const [developmentCount] = await database.query('SELECT COUNT(*) as count FROM employee_profile WHERE department = "Development"');
 
     res.render('reports', {
       femaleRatio: femaleEmployees[0].count,
       maleRatio: maleEmployees[0].count,
       availableCount: availableEmployees[0].count,
       onLeaveCount: onLeaveEmployees[0].count,
+      notEmployees: notEmployees[0].count,
+      recruitmentCount: recruitmentCount[0].count,
+      complianceCount: complianceCount[0].count,
+      compensationCount: compensationCount[0].count,
+      developmentCount: developmentCount[0].count
     });
   } catch (err) {
     console.error(err);
